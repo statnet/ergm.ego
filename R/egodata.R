454591780b08a7df538f7d79d3f08e84eb4c6632
Pavel Krivitsky <pavel@uow.edu.au>
2013-05-14 03:40:25 +0000

    * Added subsetting and indexing functions for egodata objects, as well as a sample() S3 method. * Fixed some miscellaneous bugs in ergm.ego().
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.ego/trunk@12019 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/egodata.R
+++ R/egodata.R
@@ -4,7 +4,7 @@ as.egodata <- function(object, ..., egoIDcol="egoID"){
 
 as.egodata.data.frame <- function(object, alters, egoWt = 1, ..., egoIDcol="egoID"){
   egoWt <- rep(egoWt, length.out=nrow(object))
-  out <- list(egos=object[order(object[[egoIDcol]]),], alters=.prune.alters(object, alters, egoIDcol), egoWt = egoWt[order(object[[egoIDcol]])], egoIDcol=egoIDcol)  
+  out <- list(egos=object[order(object[[egoIDcol]]),,drop=FALSE], alters=.prune.alters(object, alters, egoIDcol), egoWt = egoWt[order(object[[egoIDcol]])], egoIDcol=egoIDcol)  
   class(out) <- "egodata"
   out
 }
@@ -49,7 +49,7 @@ as.egodata.network<-function(object,special.cols=c("na","vertex.names"),...,egoI
 
   todel <- !(aeis %in% eis)
 
-  if(any(todel)) alters[!todel,]
+  if(any(todel)) alters[!todel,,drop=FALSE]
   else alters
 }
 
@@ -86,4 +86,133 @@ as.network.egodata<-function(x, N, scaling=c("greedy","sample"), ...){
     n[best]<-n[best]+1
   }
   rep(seq_along(w),n)
+}
+
+# Note: The following functions use parts of na.omit.data.frame() and
+# subset.data.frame() from the R's stats package under the terms of
+# the GNU GPL v3.
+
+`[.egodata` <- function(x, i, j, ..., dup.action=c("make.unique", "fail", "number")){
+  subset(x, i, j, ..., dup.action=dup.action)
+}
+
+subset.egodata <- function(x, subset, select, ..., dup.action=c("make.unique", "fail", "number")){
+  if(missing(subset)) subset <- TRUE
+  
+  if (missing(select)) 
+    egovars <- altervars <- TRUE
+  else {
+    if((is.numeric(select) || is.logical(select) || is.integer(select))
+       && !isTRUE(all.equal(names(x$egos),names(x$alters)))){
+      stop("Logical or numeric column subsets cannot be used if column orderings for egos and alters are not identical. Use column names instead.")
+    }
+    
+    nl <- as.list(seq_along(x$egos))
+    names(nl) <- names(x$egos)
+    vars <- eval(substitute(select), nl, parent.frame())
+    eIDi <- which(names(x$egos)==x$egoIDcol)
+    egovars <- union(eIDi,vars)
+
+    nl <- as.list(seq_along(x$alters))
+    names(nl) <- names(x$alters)
+    vars <- eval(substitute(select), nl, parent.frame())
+    eIDi <- which(names(x$alters)==x$egoIDcol)
+    altervars <- union(eIDi,vars)
+  }
+
+  egoIDs <- switch(mode(subset),
+                   numeric =,
+                   logical =,
+                   integer = x$egos[[x$egoIDcol]][subset],
+                   character = x$egos[[x$egoIDcol]][match(subset, x$egos[[x$egoIDcol]])])
+
+  egoIDs <- sort(egoIDs)
+
+  dup.action <- match.arg(dup.action)
+  unique.egoIDs <- switch(dup.action,
+                          fail=stop("Selected subset calls for duplicating egos."),
+                          numeric=seq_along(egoIDs),
+                          make.unique=make.unique(as.character(egoIDs)))
+  
+  egos <- cbind(x$egos[match(egoIDs,x$egos[[x$egoIDcol]]),egovars,drop=FALSE], .unique.egoIDs = unique.egoIDs)
+  alters <- merge(egos[c(x$egoIDcol,".unique.egoIDs")], x$alters[altervars], by=x$egoIDcol)
+  egoWt <- x$egoWt[match(egoIDs,x$egos[[x$egoIDcol]])]
+
+  # If we have duplicated egoIDs, we have to handle it per dup.action.
+  
+  if(any(duplicated(egoIDs))){
+    egos[[x$egoIDcol]] <- egos[[".unique.egoIDs"]]
+    alters[[x$egoIDcol]] <- alters[[".unique.egoIDs"]]
+  }
+  egos[[".unique.egoIDs"]] <- NULL
+  alters[[".unique.egoIDs"]] <- NULL
+
+  out <- list(egos=egos, alters=alters, egoWt = egoWt, egoIDcol=x$egoIDcol)  
+  class(out) <- "egodata"
+  out
+}
+
+# TODO: A more efficient implementation of this.
+na.omit.egodata <- function(object, relevant=TRUE, ...){
+  # Create a subdataset containing only the relevant variables.
+  obj <- subset(object,select=relevant)
+  
+  n <- length(obj$egos)
+  omit <- FALSE
+  vars <- seq_len(n)
+  for (j in vars) {
+    x <- obj$egos[[j]]
+    if (!is.atomic(x)) 
+      next
+    x <- is.na(x)
+    d <- dim(x)
+    if (is.null(d) || length(d) != 2L) 
+      omit <- omit | x
+    else for (ii in 1L:d[2L]) omit <- omit | x[, ii]
+  }
+  ego.omit <- obj$egos[[obj$egoIDcol]][omit]
+
+  n <- length(obj$alters)
+  omit <- FALSE
+  vars <- seq_len(n)
+  for (j in vars) {
+    x <- obj$alters[[j]]
+    if (!is.atomic(x)) 
+      next
+    x <- is.na(x)
+    d <- dim(x)
+    if (is.null(d) || length(d) != 2L) 
+      omit <- omit | x
+    else for (ii in 1L:d[2L]) omit <- omit | x[, ii]
+  }
+  
+  alter.omit <- obj$alters[[obj$egoIDcol]][omit]
+
+  subset(object, -match(union(ego.omit,alter.omit),object$egos[[object$egoIDcol]]))  
+}
+
+dimnames.egodata <- function(x){
+  list(x$egos[[x$egoIDcol]], names(x$egos), names(x$alters))
+}
+
+dim.egodata <- function(x){
+  c(nrow(x$egos), ncol(x$egos), ncol(x$alters))
+}
+
+# Not really a generic function, but perhaps should be.
+
+sample <- function(x, size, replace=FALSE, prob=NULL, ...) UseMethod("sample")
+sample.default <- function(x, ...) base::sample(x, ...)
+
+sample.egodata <- function(x, size, replace=FALSE, prob=NULL){
+  if(missing(size)) size <- nrow(x)
+  
+  is <- sample(seq_len(nrow(x)), size, replace, prob)
+
+  out <- subset(x, is)
+
+  if(is.null(prob)) prob <- rep(1, nrow(x))
+  
+  out$egoWt <- x$egoWt[is]/prob[is]
+  out
 }
--- R/ergm.ego.R
+++ R/ergm.ego.R
@@ -1,22 +1,28 @@
-ergm.ego <- function(formula, popsize, offset.coef=NULL, ..., control=control.ergm()){
+ergm.ego <- function(formula, popsize, ppopsize=popsize, offset.coef=NULL, na.action=na.fail, ..., control=control.ergm()){
   control$force.main <- TRUE
   egodata <- get(as.character(formula[[2]]), envir=environment(formula))
   
-  popnw <- as.network.egodata(x, popsize)
+  popnw <- as.network(egodata, ppopsize)
 
   # Get the sample h values.
   stats <- summary(remove.offset.formula(formula), individual=TRUE)
 
+  # h is just a matrix, so this will do the sensible thing.
+  w <- egodata$egoWt
+  tmp <- na.action(cbind(w,stats))
+  w <- tmp[,1]
+  stats <- tmp[,-1, drop=FALSE]
+
   # We can have summary() compute some of this, but that's likely to
   # be slow.
-  w <- egodata$egoWt/sum(egodata$egoWt)
+  w <- w/sum(w)
   m <- colSums(stats*w)
   # TODO: Include finite-population correction here:
   v <- crossprod(sweep(stats, 2, m, "-")*sqrt(w))/(1-sum(w^2))
 
-  ergm.formula <- ergm.update.formula(formula,popnw~offset(edges)+.)
+  ergm.formula <- ergm.update.formula(formula,popnw~offset(edges)+.,from.new="popnw")
 
-  ergm.fit <- ergm(ergm.formula, target.stats=m*popsize, offset.coef=c(-log(popsize),offset.coef),..., eval.loglik=FALSE,control=control)
+  ergm.fit <- ergm(ergm.formula, target.stats=m*ppopsize, offset.coef=c(-log(ppopsize/popsize),offset.coef),..., eval.loglik=FALSE,control=control)
   coef <- coef(ergm.fit)
 
   oi <- offset.info.formula(ergm.formula)
@@ -25,8 +31,9 @@ ergm.ego <- function(formula, popsize, offset.coef=NULL, ..., control=control.er
 
   vcov <- matrix(NA, length(coef), length(coef))
   
+  vcov[!oi$theta,!oi$theta] <- solve(DtDe/ppopsize)%*%v%*%solve(DtDe/ppopsize)*sum(w^2)/sum(w)^2
+
+  rownames(vcov) <- colnames(vcov) <- names(coef)
   
-  vcov[!oi$theta,!oi$theta] <- solve(DtDe/popsize)%*%v%*%solve(DtDe/popsize)
-  
-  list(coef=coef, vcov=vcov)
+  list(coef=coef, vcov=vcov, ergm.fit=ergm.fit, DtDe=DtDe, v=v)
 }
