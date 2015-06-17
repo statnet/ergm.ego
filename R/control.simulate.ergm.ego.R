eb35eaaf04152b2cd24363bb68c14f4499ec1d36
Pavel Krivitsky <pavel@uow.edu.au>
2015-06-17 11:22:11 +0000

    Fixed up simulate.ergm.ego() and its documentation: * Default value of popsize= is now more sensible. * control.simulate.ergm.ego() now has a pop.wt argument to control how the population network is reconstructed. * simulate.ergm.ego() now handles the situation where popsize is not a multiple of ppopsize of the network fit intelligently. * examples have been added throughout.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.ego/trunk@13868 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/control.simulate.ergm.ego.R
+++ R/control.simulate.ergm.ego.R
@@ -1,11 +1,19 @@
-control.simulate.ergm.ego <- function(SAN.control = control.san(),
-                                      simulate.control = control.simulate(),
-                                      ...){
+control.simulate.ergm.ego <- function(
+  ppop.wt = c("round","sample"),
+  SAN.control = control.san(),
+  simulate.control = control.simulate(),
+  ...){
+  match.arg.pars <- c("ppop.wt")
+
   control<-list()
   formal.args<-formals(sys.function())
   formal.args[["..."]]<-NULL
   for(arg in names(formal.args))
     control[arg]<-list(get(arg))
   if(length(list(...))) stop("Unrecognized control parameter: ",arg,".")
+
+  for(arg in match.arg.pars)
+    control[arg]<-list(match.arg(control[[arg]][1],eval(formal.args[[arg]])))
+
   set.control.class("control.simulate.ergm.ego")
 }
--- R/simulate.ergm.ego.R
+++ R/simulate.ergm.ego.R
@@ -1,11 +1,23 @@
-simulate.ergm.ego <- function(object, nsim = 1, seed = NULL, popsize=object$popsize, control=control.simulate.ergm.ego(), ..., verbose=FALSE){
+simulate.ergm.ego <- function(object, nsim = 1, seed = NULL, popsize=if(object$popsize==1) object$ppopsize else object$popsize, control=control.simulate.ergm.ego(), ..., verbose=FALSE){
+  check.control.class()
+  
   egodata <- object$egodata
-  popnw <- as.network(egodata, popsize)
+  popnw <- as.network(egodata, popsize, scaling=control$ppop.wt)
+  ppopsize <- if(network.size(popnw)!=popsize){
+    message("Note: Constructed network has size ", network.size(popnw), " different from requested ", popsize,". Simulated statistics may need to be rescaled.")
+    network.size(popnw)
+  }else popsize
+
+  
   ergm.formula <- ergm.update.formula(object$formula,popnw~.,from.new="popnw")
 
-  popnw <- san(ergm.formula, target.stats = object$ergm.fit$target.stats[-1]/object$popsize*popsize,verbose=verbose, control=control$SAN.control, ...)
-  ergm.formula <- ergm.update.formula(object$formula,popnw~edges+.,from.new="popnw")
+  popnw <- san(ergm.formula, target.stats = object$target.stats[-1]/object$ppopsize*ppopsize,verbose=verbose, control=control$SAN.control, ...)
+  ergm.formula <- ergm.update.formula(object$formula,popnw~netsize.adj+.,from.new="popnw")
 
-  out <- simulate(ergm.formula, nsim=nsim, seed=seed, verbose=verbose, coef=c(-log(popsize/object$popsize),object$coef), control=control$simulate.control, ...)
-  if(is.matrix(out)) out <- out[,-1,drop=FALSE]
+  out <- simulate(ergm.formula, nsim=nsim, seed=seed, verbose=verbose, coef=c(netsize.adj=-log(ppopsize/object$popsize),object$coef[-1]), control=control$simulate.control, ...)
+  if(is.matrix(out)){
+    out <- out[,-1,drop=FALSE]
+    attr(out, "ppopsize") <- ppopsize
+  }
+  out
 }
