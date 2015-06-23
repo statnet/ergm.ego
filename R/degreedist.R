93daef98202ffcc97de49246aecfa10e94bfd0fd
Kirk Li <kirkli@uw.edu>
2015-06-23 01:33:51 +0000

    from Emily: fix degreedist and mixingmatrix to include ego weights
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.ego/trunk@13921 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/degreedist.R
+++ R/degreedist.R
@@ -4,13 +4,26 @@ degreedist.egodata <- function(egodata, freq = FALSE, prob = !freq,
     stop("The egodata object passed to degreedist.egodata must be of class egodata.")
   }
   color <- "#83B6E1"
-  beside <- FALSE
-  maxdeg <- max(table(egodata$alters["vertex.names"]))
+  beside <- TRUE
+  ylabel <- "Frequency"
+  egoIDcol <- egodata$egoIDcol
+  degtable <- rep(0, nrow(egodata$egos))
+  degtable[as.numeric(names(table(egodata$alters[egoIDcol])))] <- table(egodata$alters[egoIDcol])
+  degtable.wt <- degtable * egodata$egoWt
+  maxdeg <- max(degtable.wt)
   deg.ego <- summary(egodata ~ degree(0:maxdeg, by = by))
   names(deg.ego) <- 0:maxdeg
+  maxfreq <- max(deg.ego)
   if(!is.null(by)){
-    deg.ego <- matrix(deg.ego, ncol = maxdeg + 1, byrow = TRUE)
+    levs <- sort(unique(c(egodata$egos[[by]], egodata$alters[[by]])))
+    deg.ego <- matrix(0, nrow = length(levs), ncol = maxdeg + 1)
+    rownames(deg.ego) <- levs
     colnames(deg.ego) <- 0:maxdeg
+    for(i in 1:length(levs)){
+      vals <- table(degtable.wt[egodata$egos[by] == levs[i]])
+      toreplace <- as.numeric(names(vals)) + 1
+      deg.ego[i, toreplace] <- vals
+    }
     ncolors <- dim(deg.ego)[1]
     if(ncolors == 2){
       color <- c("#eff3ff", "#377FBC")
@@ -24,39 +37,62 @@ degreedist.egodata <- function(egodata, freq = FALSE, prob = !freq,
     lfill <- c(color, 0)
     lborder <- c(rep("black", times = ncolors), 0)
     ltitle <- by
+    maxfreq <- max(colSums(deg.ego))
   }
   if(prob){
     if(is.null(by)){
-      deg.ego <- deg.ego/sum(deg.ego)
+      scaledeg <- sum(deg.ego)
+      deg.ego <- deg.ego/scaledeg
+      maxfreq <- max(deg.ego, na.rm = TRUE)
     } else {
-      deg.ego <- t(deg.ego)/colSums(deg.ego)
-      deg.ego <- t(deg.ego)
+      scaledeg <- rowSums(deg.ego)
+      deg.ego <- deg.ego/scaledeg
+      deg.ego <- deg.ego
+      maxfreq <- max(max(deg.ego, na.rm = TRUE))
       beside <- TRUE
     }
   }
-  maxfreq <- ifelse(is.null(by), yes = max(deg.ego, na.rm = TRUE), 
-                    no = max(colSums(deg.ego), na.rm = TRUE))
 
 
-  if (brgmod) {
+  if(brgmod) {
     brgdraws <- simulate.ergm.ego(ergm.ego(egodata ~ edges), nsim = 50)
     deg.brg <- summary(brgdraws ~ degree(0:maxdeg))
     brgmeans <- apply(deg.brg, MARGIN = 2, FUN = mean)
     brgsd <- apply(deg.brg, MARGIN = 2, FUN = sd)
     upper <- brgmeans + 2 * brgsd
     lower <- brgmeans - 2 * brgsd
+    
+    if(prob){
+      if(is.null(by)){
+        brgmeans <- brgmeans/scaledeg
+        upper <- upper/scaledeg
+        lower <- lower/scaledeg
+        ylabel <- "Probability"
+      } else {
+        upper <- upper/sum(brgmeans)
+        lower <- lower/sum(brgmeans)
+        brgmeans <- brgmeans/sum(brgmeans)
+        ylabel <- "Probability (within attr level)"
+      }
+      
+    }
     maxfreq <- max(maxfreq, upper, na.rm = TRUE)
   }
   
-  baraxis <- barplot(deg.ego, xlab = "Degree", ylab = NULL,
+  baraxis <- barplot(deg.ego, xlab = "Degree", ylab = ylabel,
                      col = color, beside = beside, plot = TRUE,
                      ylim = c(0, maxfreq))
   
   if(brgmod){
-    points(x = baraxis-.15, y = brgmeans, col = "firebrick",
+    baraxis <- if(is.null(by)){
+      baraxis - 0.15
+    } else {
+      colMeans(baraxis)
+    }
+    points(x = baraxis, y = brgmeans, col = "firebrick",
            lwd = 1, pch = 18, cex = 1.25)
-    suppressWarnings(arrows(x0 = baraxis-.15, y0 = upper,
-                            x1 = baraxis-.15, y1 = lower,
+    suppressWarnings(arrows(x0 = baraxis, y0 = upper,
+                            x1 = baraxis, y1 = lower,
                             code = 3, length = 0.1, 
                             angle = 90, col = "firebrick"))
   } 
@@ -72,13 +108,20 @@ mixingmatrix.egodata <- function(egodata, attrname, rowprob = FALSE){
   alters <- egodata$alters
   egoIDcol <- egodata$egoIDcol
   
-  levs <- sort(unique(c(egos[[attrname]],alters[[attrname]])))
+  levs <- sort(unique(c(egos[[attrname]], alters[[attrname]])))
   egos[[attrname]] <- match(egos[[attrname]], levs, 0)
   alters[[attrname]] <- match(alters[[attrname]], levs, 0)
   
   ties <- merge(egos[c(egoIDcol,attrname)], alters[c(egoIDcol,attrname)], 
                 by = egoIDcol, suffixes = c(".ego",".alter"))
-  mxmat <- table(ties[,paste0(attrname, c(".ego", ".alter"))])
+  ties$wt <- egodata$egoWt[ties$vertex.names]
+  mxmat <- matrix(0, nrow = length(levs), ncol = length(levs))
+  
+  for(i in 1:length(levs)){
+    for(j in 1:length(levs)){
+      mxmat[i,j] <- sum(ties$wt[ties[,2]==i & ties[,3]==j])
+    }
+  }
   dimnames(mxmat) <- list(ego = levs,  
                           alter = levs)
   if(rowprob){
