64f6a7643bd9666c9cf2b901630a184328631f9e
Pavel Krivitsky <pavel@uow.edu.au>
2016-03-16 06:32:44 +0000

    Commented out reweight.egodata(). These probably shouldn't be released at this point, if ever.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.ego/trunk@14316 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/reweight.egodata.R
+++ R/reweight.egodata.R
@@ -1,23 +1,23 @@
-reweight.egodata <- function(x, g, gw){
-  gw <- gw[order(gw[,1]),2]
+## reweight.egodata <- function(x, g, gw){
+##   gw <- gw[order(gw[,1]),2]
   
-  w0 <- x$egoWt
+##   w0 <- x$egoWt
 
-  # aggregate will sort by g.
-  gw0 <- aggregate(w0~g, FUN=sum)
+##   # aggregate will sort by g.
+##   gw0 <- aggregate(w0~g, FUN=sum)
 
-  gwm <- setNames(gw/gw0[,2],gw0[,1])
+##   gwm <- setNames(gw/gw0[,2],gw0[,1])
     
-  x$egoWt <- x$egoWt*gwm[g]
+##   x$egoWt <- x$egoWt*gwm[g]
 
-  x
-}
+##   x
+## }
 
-category.weights.egodata <- function(x, pop, by){
-  x.g <- apply(x$egos[by],1,paste,collapse="\n")
-  pop.g <- apply(pop$egos[by],1,paste,collapse="\n")
+## category.weights.egodata <- function(x, pop, by){
+##   x.g <- apply(x$egos[by],1,paste,collapse="\n")
+##   pop.g <- apply(pop$egos[by],1,paste,collapse="\n")
 
-  pop.freq <- as.data.frame(table(pop.g))
+##   pop.freq <- as.data.frame(table(pop.g))
 
-  list(g=x.g, gw=pop.freq)
-}
+##   list(g=x.g, gw=pop.freq)
+## }
