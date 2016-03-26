93fb169240cea1d2f88810fc07525c65c2e71ea3
Pavel Krivitsky <pavel@uow.edu.au>
2016-03-26 12:33:24 +0000

    Added copyright headers (for real this time).
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/ergm.ego/trunk@14354 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/EgoStat.duration.R
+++ R/EgoStat.duration.R
@@ -1,3 +1,12 @@
+#  File R/EgoStat.duration.R in package ergm.ego, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2015-2016 Statnet Commons
+#######################################################################
 EgoStat.mean.age <- function(egodata, emptyval=0){
   startcol <- egodata$startcol
   if(is.null(startcol)) stop("Egocentric dataset does not appear to contain durational information.")
