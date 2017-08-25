c3f1689eeaf2fca8c375bd5be5c2379054c0dfe4
Michal Bojanowski <michal2992@gmail.com>
2017-08-25 19:25:18 +0200

    More mixingmatrix() tests
    Testing mm from factors.
    Testing mm level order.

--- tests/testthat/test-mixingmatrix.R
+++ tests/testthat/test-mixingmatrix.R
@@ -27,3 +27,54 @@ for( v in varnames ) {
       expect_identical( colnames(mm), levels(x))
     })  
 }
+
+rm(varnames, v)
+
+
+
+
+
+
+
+context("Testing mixingmatrix() on different types of attributes")
+
+set.seed(666)
+egodf <- dplyr::data_frame(
+  id_ego = seq(1, 5),
+  num = rnorm(max(id_ego)),
+  int = as.integer(sample(1:3, max(id_ego), replace=TRUE)),
+  ch = sample(letters[1:3], max(id_ego), replace=TRUE),
+  fac = factor(sample(LETTERS[1:3], max(id_ego), replace=TRUE), levels=LETTERS[1:3])
+)
+degs <- rpois(nrow(egodf), 2)
+alterdf <- dplyr::data_frame(
+  id_ego = rep(egodf$id_ego, degs),
+  id_alter = unlist(lapply(degs[degs>0], function(x) seq(1, x)))
+) %>%
+  dplyr::mutate(
+    num = rnorm(n()),
+    int = as.integer(sample(1:3, n(), replace=TRUE)),
+    ch = sample(letters[1:4], n(), replace=TRUE),
+    fac = factor(sample(LETTERS[1:4], n(), replace=TRUE), levels=LETTERS[4:1])
+  )
+edata <- egor::egor(alterdf, egodf, ID.vars=list(ego="id_ego", alter="id_alter"))
+
+varnames <- c("int", "ch", "fac")
+for ( v in varnames ) {
+  test_that(
+    paste0(
+      "Sum of mixing matrix for ", 
+      sQuote(v), 
+      " is equal to the total number of alters"
+    ), {
+      expect_silent(mm <- mixingmatrix(edata, v))
+      expect_equal(sum(mm), sum(vapply(edata$.alts, nrow, numeric(1))))
+    })  
+}
+
+test_that("Rows of mm are properly ordered for factors", {
+  x <- dplyr::pull(edata, "fac")
+  expect_silent( mm <- mixingmatrix(edata, "fac"))
+  expect_identical( rownames(mm), levels(x))
+})
+
