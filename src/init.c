07e4beec87423f0d8d3549f3ee2c93c0e0eb05b4
Pavel N. Krivitsky <pavel@uow.edu.au>
2017-08-29 16:56:25 +1000

    Added (empty) native C routine registration and symbol search settings.

---
+++ src/init.c
@@ -0,0 +1,9 @@
+static const R_CMethodDef CEntries[] = {
+    {NULL, NULL, 0}
+};
+
+void R_init_ergm_ego(DllInfo *dll)
+{
+    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_useDynamicSymbols(dll, TRUE);
+}
