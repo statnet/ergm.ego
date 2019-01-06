75bdd568bca4b94c595bf6fbe8c998d8f93f749f
Michal Bojanowski <michal2992@gmail.com>
2019-01-06 18:43:24 +0100

    Fix custom data degreedist() test

--- tests/testthat/test-degreedist.R
+++ tests/testthat/test-degreedist.R
@@ -5,8 +5,6 @@ library(egor)
 
 test_that("degreedist() gives correct results on custom data", {
   
-  skip("Not ready yet")
-  
   library(tibble)
   
   ## |----|--------|---|----------|-----------|
@@ -18,41 +16,60 @@ test_that("degreedist() gives correct results on custom data", {
   ## | 4  | 2      | b | 2        | a, b      |
   ## |----|--------|---|----------|-----------|
   
-  e <- egor(list(tibble(x="a"),
-                 tibble(x=c("b","a")),
-                 tibble(x="b"),
-                 tibble(x=c("a","b"))),
-            tibble(x=letters[c(1,1,2,2)]),
-            ego_design=list(~1,weights=c(2,1,1,2)))
-  
-  expect_equivalent(unclass(degreedist(e, plot=FALSE)), c(1/2,1/2))
-  
+  e <- egor(
+    list(
+      tibble(x="a"),
+      tibble(x=c("b","a")),
+      tibble(x="b"),
+      tibble(x=c("a","b"))
+    ),
+    tibble(y=letters[c(1,1,2,2)]),
+    ID.vars = list(ego="x", alter="x"),
+    ego_design=list(~1,weights=c(2,1,1,2))
+  )
 
-  expect_equivalent(unclass(degreedist(e, plot=FALSE, by="x")), rbind(c(2/3,1/3),
-                                                                      c(1/3,2/3)))
+  expect_equivalent(
+    unclass(degreedist(e, plot=FALSE)), 
+    c(1/2,1/2)
+  )
+
+  expect_equivalent(
+    unclass(degreedist(e, plot=FALSE, by="y")), 
+    rbind(c(2/3,1/3), c(1/3,2/3))
+  )
 })
 
 
 
 
+
+
+
+# Tests using egor:egor32 data --------------------------------------------
+
+
+data("egor32", package="egor")
+
+
 test_that("degreedist() works on egor::egor32 data", {
-  data("egor32", package="egor")
   expect_silent(degreedist(egor32))
 })
 
 test_that("degreedist() works on egor::egor32 data with `by=sex` (a factor)", {
-  data("egor32", package="egor")
   expect_silent(degreedist(egor32, by="sex"))
 })
 
+
+
+# Tests using ergm::faux.mesa.high data -----------------------------------
+
+data(faux.mesa.high, package="ergm")
+fmh.ego <- as.egor(faux.mesa.high)
+
 test_that("degreedist() works on data based on faux.mesa.high", {
-  data(faux.mesa.high, package="ergm")
-  fmh.ego <- as.egor(faux.mesa.high)
   expect_silent(degreedist(fmh.ego))
 })
 
 test_that("degreedist() works on data based on faux.mesa.high with `by=Sex`", {
-  data(faux.mesa.high, package="ergm")
-  fmh.ego <- egor::as.egor(faux.mesa.high)
   expect_silent(degreedist(fmh.ego, by="Sex"))
 })
