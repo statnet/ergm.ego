0482617f9bc1fd00c93685096b5fe1c02d7dac2b
Pavel N. Krivitsky <pavel@uow.edu.au>
2019-05-29 15:58:27 +1000

    Updated LICENSE year and, affilliation, and copyright headers.

--- R/EgoStat.R
+++ R/EgoStat.R
@@ -1,11 +1,11 @@
 #  File R/EgoStat.R in package ergm.ego, part of the Statnet suite
-#  of packages for network analysis, http://statnet.org .
+#  of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
-#  http://statnet.org/attribution
+#  https://statnet.org/attribution
 #
-#  Copyright 2015-2018 Statnet Commons
+#  Copyright 2015-2019 Statnet Commons
 #######################################################################
 # An EgoStat.* function takes the data frame of egos, a data frame of
 # alters, and the arguments passed to the corresponding ERGM terms,
--- R/summary.ergm.ego.R
+++ R/summary.ergm.ego.R
@@ -1,11 +1,11 @@
 #  File R/summary.ergm.ego.R in package ergm.ego, part of the Statnet suite
-#  of packages for network analysis, http://statnet.org .
+#  of packages for network analysis, https://statnet.org .
 #
 #  This software is distributed under the GPL-3 license.  It is free,
 #  open source, and has the attribution requirements (GPL Section 7) at
-#  http://statnet.org/attribution
+#  https://statnet.org/attribution
 #
-#  Copyright 2015-2018 Statnet Commons
+#  Copyright 2015-2019 Statnet Commons
 #######################################################################
 ## A thin wrapper around summary.ergm to get rid of a spurious error message.
 #' @method summary ergm.ego
