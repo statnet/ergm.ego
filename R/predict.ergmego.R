d994021e7579a92dd0fc55a5be9355fbcc3a835a
Michal Bojanowski <michal2992@gmail.com>
2020-09-26 21:41:20 +0200

    Add predict.ergm.ego()
    References statnet/ergm.ego#46.

---
+++ R/predict.ergmego.R
@@ -0,0 +1,19 @@
+#' ERGM-based predicted tie probabilities for the pseudo-population network
+#' 
+#' @param object model fit as returned by [ergm.ego()]
+#' @param ... other arguments passed to/from other methods
+#' 
+#' @return See [ergm::predict.ergm()]
+#' 
+#' @export
+
+predict.ergm.ego <- function(object, ...) {
+  # Extract network
+  net <- object$network
+  # Update formula with pseudo-population network
+  frm <- statnet.common::nonsimp_update.formula(object$formula, net ~ .)
+  assign("net", object$network, envir=environment(frm))
+  # thetas without the offset(s)
+  th <- ergm.eta(object$coef, object$etamap)[!object$etamap$offsettheta]
+  predict(frm, theta=th, ...)
+}
