ae8ecb3a2064234ee3840a1f3e107cddbba020eb
Michal Bojanowski <michal2992@gmail.com>
2020-09-29 17:17:38 +0200

    Another doc link fix

--- R/EgoStat.node.attr.R
+++ R/EgoStat.node.attr.R
@@ -10,11 +10,10 @@
 #' @name node-attr-api
 #' @title Helper functions for specifying nodal attribute levels
 #'
-#' @description These functions are meant to be used in `EgoStat` and
-#'   other implementations to provide the user with a way to extract
-#'   nodal attributes and select their levels in standardized and
-#'   flexible ways described under [node-attr]. They are intended to
-#'   parallel [node-attr-api] of `ergm` package.
+#' @description These functions are meant to be used in `EgoStat` and other
+#'   implementations to provide the user with a way to extract nodal attributes
+#'   and select their levels in standardized and flexible ways. They are
+#'   intended to parallel [ergm::nodal_attributes-API] of `ergm` package.
 #'
 #' @param object An argument specifying the nodal attribute to select
 #'   or which levels to include.
