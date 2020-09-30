b2f4c757df15a9b381f43c4d0d72f138030586bf
Michal Bojanowski <michal2992@gmail.com>
2020-09-30 20:23:53 +0200

    More predict.ergm.ego() tests

--- tests/testthat/test-predict.ergm.ego.R
+++ tests/testthat/test-predict.ergm.ego.R
@@ -1,6 +1,6 @@
 context("Test predict.ergm.ego()")
 
-test_that("it works", {
+test_that("it just works for model without offsets", {
   data(faux.mesa.high, package="ergm")
   fmh.ego <- as.egodata(faux.mesa.high)
   egofit <- ergm.ego(
@@ -9,6 +9,26 @@ test_that("it works", {
     popsize=network.size(faux.mesa.high)
   )
   expect_silent(
-    predict(egofit)
+    p <- predict(egofit)
   )
+  expect_true(all(is.finite(p$p)))
+})
+
+
+test_that("it just works for model with offsets", {
+  data("faux.magnolia.high", package="ergm")
+  fmhego <- as.egodata(faux.magnolia.high)
+  fit <- ergm.ego(
+    fmhego ~ edges 
+    + nodefactor("Grade")
+    + nodematch("Grade", diff=T)
+    + offset(nodematch("Sex",
+                       diff = TRUE,
+                       levels = c(1, 2))),
+    offset.coef = rep(-Inf, 2)
+  )
+  expect_silent(
+    p <- predict(fit) # data frame
+  )
+  expect_true(all(is.finite(p$p)))
 })
