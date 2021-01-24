581bf980bea665860e4655bec9fa3e188af3d878
Michal Bojanowski <michal2992@gmail.com>
2021-01-24 02:43:46 +0100

    Update tests
    Move example code to tests keeping part of it for demonstration
    purposes.

--- R/summary.statistics.egodata.R
+++ R/summary.statistics.egodata.R
@@ -54,14 +54,8 @@
 #' 
 #' data(faux.mesa.high)
 #' fmh.ego <- as.egodata(faux.mesa.high)
-#' (nw.summ <- summary(faux.mesa.high~edges+degree(0:3)+nodematch("Race")+
-#'                     nodematch("Sex")+absdiff("Grade")+nodemix("Grade")))
-#' 
-#' (ego.summ <- summary(fmh.ego~edges+degree(0:3)+nodematch("Race")+nodematch("Sex")+
-#'                      absdiff("Grade")+nodemix("Grade"),
-#'                      scaleto=network.size(faux.mesa.high)))
-#' 
-#' stopifnot(isTRUE(all.equal(nw.summ,ego.summ)))
+#' summary(faux.mesa.high~edges+degree(0:3)+nodematch("Race")+
+#'                     nodematch("Sex")+absdiff("Grade")+nodemix("Grade"))
 #' 
 #' @export
 summary_formula.egodata <- function(object,..., basis=NULL, individual=FALSE, scaleto=NULL){
