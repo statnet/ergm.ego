2f50bdb4817a484b30ee0f1676ce862d76867eb1
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2021-02-18 19:33:22 +1100

    Plugged the package into the snctrl() API.
    references statnet/statnet.common#14

--- R/zzz.R
+++ R/zzz.R
@@ -13,10 +13,31 @@
   if(!is.null(sm)){
     packageStartupMessage(sm)
   }
-  
 }
 
-.onLoad <- function(lib, pkg){
-  # . is used in purrr and elsewhere.
+.onLoad <- function(libname, pkgname){
+  # . is used as a placeholder by stantet.common::NVL3().
   utils::globalVariables(".")
+
+  eval(COLLATE_ALL_MY_CONTROLS_EXPR)
 }
+
+# TODO: Figure out some automatic way to keep this in sync with statnet.common.
+#' @name snctrl
+#'
+#' @title Statnet Control
+#'
+#' @description A utility to facilitate argument completion of control lists, reexported from `statnet.common`.
+#'
+#' @section Currently recognised control parameters:
+#' This list is updated as packages are loaded and unloaded.
+#'
+#' \Sexpr[results=rd,stage=render]{statnet.common::snctrl_names()}
+#'
+#' @seealso [statnet.common::snctrl()]
+#' @docType import
+NULL
+#' @export
+snctrl <- statnet.common::snctrl
+
+eval(UPDATE_MY_SCTRL_EXPR)
