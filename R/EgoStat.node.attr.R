2c1c4d9cda46b57a3b8587eebd9e89a0232277c8
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2025-06-10 18:57:16 +1000

    Roxygenized and updated copyright headers.

--- R/EgoStat.node.attr.R
+++ R/EgoStat.node.attr.R
@@ -1,8 +1,8 @@
-#  File R/EgoStat.node.attr.R in package ergm.ego, part of the
-#  Statnet suite of packages for network analysis, https://statnet.org .
+#  File R/EgoStat.node.attr.R in package ergm.ego, part of the Statnet suite of
+#  packages for network analysis, https://statnet.org .
 #
-#  This software is distributed under the GPL-3 license.  It is free,
-#  open source, and has the attribution requirements (GPL Section 7) at
+#  This software is distributed under the GPL-3 license.  It is free, open
+#  source, and has the attribution requirements (GPL Section 7) at
 #  https://statnet.org/attribution .
 #
 #  Copyright 2015-2025 Statnet Commons
